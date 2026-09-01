ef8135dfa6158e95208e6c02c829b4360beaa0a3
Subrahmanya <codewithsubrahmanya@gmail.com>
2026-09-01 15:24:24 +0530

    Add DSA code

---
+++ patterns/Main.java
@@ -0,0 +1,13 @@
+
+class Main {
+
+    public static void main(String[] args) {
+        int row = 4;
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= row; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
