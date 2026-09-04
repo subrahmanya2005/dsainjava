aa177c3023dd1a3c9a60a8d2929ae8d31713e12f
Subrahmanya <codewithsubrahmanya@gmail.com>
2026-09-04 20:53:39 +0530

    Added

---
+++ patterns/Main12.java
@@ -0,0 +1,20 @@
+
+class Main12 {
+
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
