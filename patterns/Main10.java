320a4093472d479ebd43b7c07231ea8c025c7cff
Subrahmanya <codewithsubrahmanya@gmail.com>
2026-09-04 20:06:35 +0530

    Added

--- patterns/Main10.java
+++ patterns/Main10.java
@@ -1,11 +1,11 @@
 class Main10{
     public static void main(String[] args) {
-        int n=5;
-        for (int i = 0; i < 5; i++) {
+        int n=9;
+        for (int i = 0; i < n; i++) {
           for (int j = 0; j < i; j++) {
               System.out.print(" ");
           }  
-          for (int j= 0;j < 9-(i+i); j++) {
+          for (int j= 0;j < ((n+n)-1)-(i+i); j++) {
               System.out.print("*");
           }
           System.out.println();
