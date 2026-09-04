98db7516bc7c9afbf9512844184b704f7fb7454d
Subrahmanya <codewithsubrahmanya@gmail.com>
2026-09-04 19:38:07 +0530

    Added

---
+++ patterns/Main6.java
@@ -0,0 +1,13 @@
+
+class Main6 {
+
+    public static void main(String[] args) {
+        int row = 4;
+        for (int i = row; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ patterns/Main9.java
@@ -0,0 +1,17 @@
+
+class Main9 {
+
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j <= n - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
